433fa88e8e5a841dd67c77d4b93b6ab0f8ad9271
chairul <chairul.amri@ebconnect.com>
2024-01-22 11:41:25 +0700

    Initial commit

---
+++ app/src/main/java/com/idcamp/testingapps/unittest/junit/MyUnitTestActivity.kt
@@ -0,0 +1,12 @@
+package com.idcamp.testingapps.unittest.junit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.idcamp.testingapps.R
+
+class MyUnitTestActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_unit_test)
+    }
+}
