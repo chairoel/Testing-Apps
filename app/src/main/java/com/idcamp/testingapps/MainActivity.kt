5e0cf214e0313074955148a066b24205cbc7b77e
chairul <chairul.amri@ebconnect.com>
2024-01-22 11:53:19 +0700

    [update] Preparation for Codelab Unit Test Using Mockito

--- app/src/main/java/com/idcamp/testingapps/MainActivity.kt
+++ app/src/main/java/com/idcamp/testingapps/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.idcamp.testingapps
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.idcamp.testingapps.databinding.ActivityMainBinding
+import com.idcamp.testingapps.unittest.junit.MyUnitTestActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnMyUnitTest.setOnClickListener {
+            startActivity(Intent(this@MainActivity, MyUnitTestActivity::class.java))
+        }
     }
 }
